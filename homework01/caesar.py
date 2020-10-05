306735bf3d9ec84f7951afb67bf5db366cec4a76
AnnaKosten <ann.kostennn@gmail.com>
2020-10-05 22:07:14 +0300

    Реализованы функции encrypt_caesar() и decrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,7 +15,17 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    for letter in plaintext:
+        if ord(letter) in range(65, 88) or ord(letter) in range(97, 120):
+            ch = ord(letter) + shift
+            chletter = chr(ch)
+        elif ord(letter) in range(88, 91) or ord(letter) in range(120, 123):
+            ch = ord(letter) - 26 + shift
+            chletter = chr(ch)
+        else:
+            chletter = letter
+        ciphertext += chletter
     return ciphertext
 
 
@@ -33,7 +43,17 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    for letter in ciphertext:
+        if ord(letter) in range(68, 91) or ord(letter) in range(100, 123):
+            pl = ord(letter) - shift
+            pletter = chr(pl)
+        elif ord(letter) in range(65, 68) or ord(letter) in range(97, 100):
+            pl = ord(letter) + 26 - shift
+            pletter = chr(pl)
+        else:
+            pletter = letter
+        plaintext += pletter
     return plaintext
 
 
